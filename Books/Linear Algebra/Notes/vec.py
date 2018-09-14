e4d0f53affe90b2ce790b5f1e0cf3aaec49485fa
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-09-14 17:22:43 +0300

    Added if statement to account for a negative angle

--- Books/Linear Algebra/Notes/vec.py
+++ Books/Linear Algebra/Notes/vec.py
@@ -19,6 +19,15 @@ def angle_mag(mag_1, ang_1, mag_2, ang_2):
   Args:
     mag_1, ang_1 : magnitude and angle of vector 1
     mag_2, ang_2 : magnitude and angle of vector 2
+
+  Methods:
+    cos(x): returns the cosine of x in radians
+    sin(x): returns the sine of x in radians
+    radians(x): takes in a degree x and returns radians
+    hypot(x, y): returns the length of a vector from origin to point(x, y) ie. sqrt(x*x, y*y)
+    degrees(): returns the a degree from radians
+    atan2(): takes in the points x and y and returns the angle in the correct quadrant.
+
   """
   # Stores the x cordinate for our new vector
   x_component = mag_1*cos(radians(ang_1)) + mag_2*cos(radians(ang_2))
@@ -30,6 +39,9 @@ def angle_mag(mag_1, ang_1, mag_2, ang_2):
 
   # Stores the angle of our new vector. Note that we begin with y
   new_ang = degrees(atan2(y_component, x_component))
+  #  returns the positive angle made with the x-axis
+  if new_ang < 0:
+    new_ang = 360 + new_ang
 
   print(f"It is {isinstance(mag_1, int)}. {mag_1} is a of type {type(mag_1).__name__}.")
 
